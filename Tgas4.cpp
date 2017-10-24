ae08077ddcc1ac3dd19ebd33feab73a279575538
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2017-10-24 06:45:41 +0100

    Menginisialisasi nilai

--- Tgas4.cpp
+++ Tgas4.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 
 int main (){
-	
+	double LuasRumah = 42;
+	double LantaiPkt = 193*1215;
+	double LuasBox = LantaiPkt*8/1000000;
+	double JumlahBoxP, BiayaP, BiayaPG, TotalB;
 }
